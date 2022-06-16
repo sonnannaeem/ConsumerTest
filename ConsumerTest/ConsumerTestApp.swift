da7a66dba3510c9d56ec7afe2793c8f0a1f1ea17
Sonnan Naeem <sonnan.naeem@onymos.com>
2022-06-16 12:05:02 -0700

    Ready state SDK.doSomeWork()

--- ConsumerTest/ConsumerTestApp.swift
+++ ConsumerTest/ConsumerTestApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import TestCoreFramework
 
 @main
 struct ConsumerTestApp: App {
+    init() {
+        SDK.sayHello()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
