70f1ca97bb48d6e4ba911eb8b90bf9ae0dcd13ad
Sonnan Naeem <sonnan.naeem@onymos.com>
2022-06-15 19:06:04 -0700

    Initial Commit

---
+++ ConsumerTest/ConsumerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumerTestApp.swift
+//  ConsumerTest
+//
+//  Created by Sonnan Naeem on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ConsumerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConsumerTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConsumerTest
+//
+//  Created by Sonnan Naeem on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
